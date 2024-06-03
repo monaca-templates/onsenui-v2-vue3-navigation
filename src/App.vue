b25ece792c3af9f5bfdfec7d101fda24aca3b447
knaito-asial <knaito@asial.co.jp>
2024-06-03 15:48:42 +0900

    feat: fix animation

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,13 @@
 <template>
   <v-ons-navigator v-model:page-stack="pageStack"
     @push="pageStack = [...pageStack, $event]"
-    @pop="packStage = pageStack.slice(0, -1)"
+    @pop="pageStack = pageStack.slice(0, -1)"
   >
   </v-ons-navigator>
 </template>
 
 <script>
   import page1 from './Page1.vue';
-  import page2 from './Page2.vue';
 
   export default {
     data() {
